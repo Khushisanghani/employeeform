94f9b0469911679615f94fdfc041de35bd3c3c2c
khushisanghani <khushisanghani2503@gmail.com>
2024-12-26 19:32:09 +0530

    update table

--- src/Components/EmpForm.js
+++ src/Components/EmpForm.js
@@ -3,11 +3,11 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {  useEffect, useState } from 'react';
-import TableView from './TableView';
+// import TableView from './TableView';
 
 
 const getdatafromLS = () => {
-    const data = localStorage.getItem('SubmitData');
+    const data = localStorage.getItem('Submit');
     if(data){
         return JSON.parse(data);
     }
@@ -16,6 +16,7 @@ const getdatafromLS = () => {
     }
 }
 function EmpForm(){
+    
     const [EmployData,setEmployData] = useState({
         name:'',
         designation:'',
@@ -24,14 +25,25 @@ function EmpForm(){
         department:'',
         status:'',
     });
+    
+   
     const[SubmitData,setSubmitData] = useState(getdatafromLS());
     const handleinputchange = (e) => {
         
         setEmployData({...EmployData,[e.target.name]:e.target.value});
     }
-    
+    const[EditData,setEditData] = useState(null);
     const handlesubmit = (e) =>{
         e.preventDefault();
+        if(EditData !== null){
+            const Update = [...SubmitData];
+            Update[EditData]=EmployData;
+            setSubmitData(Update);
+            setEditData(null);
+        }
+        else{
+            setSubmitData((prevdata) => [...prevdata,EmployData]);
+        }
         setSubmitData([...SubmitData,EmployData]);
         setEmployData({
             name:'',
@@ -42,24 +54,33 @@ function EmpForm(){
             status:'',
         });
     };
-    const deletedata = (contact) =>{
-        setEmployData(SubmitData.filter(data => data.contact !== contact ));
+    const deletedata = (targetindex) =>{
+        setSubmitData(SubmitData.filter((_,index) => index !== targetindex));
     }
+    const handleEdit = (index) => {
+        const Updatedata = SubmitData[index];
+        setSubmitData(Updatedata);
+        setEditData(index);
+    }
+    
+    
     useEffect(()=>{
-        localStorage.setItem('SubmitData',JSON.stringify(SubmitData));
+        localStorage.setItem('Submit',JSON.stringify(SubmitData));
     },[SubmitData])
+
+    
     return(
         <>
         <Container className='mt-2'>
         <Form onSubmit={handlesubmit}>
         <Form.Label >Name :</Form.Label>
-        <Form.Control type="text" name="name" value={EmployData.name} onChange={handleinputchange}/>
+        <Form.Control type="text" name="name" value={EmployData.name} onChange={handleinputchange} required autoComplete='off'/>
         <Form.Label >Designation :</Form.Label>
-        <Form.Control type="text" name="designation" value={EmployData.designation} onChange={handleinputchange}/>
+        <Form.Control type="text" name="designation" value={EmployData.designation} onChange={handleinputchange} required autoComplete='off'/>
         <Form.Label >Address :</Form.Label>
-        <Form.Control as="textarea" rows={2} name='address' value={EmployData.address} onChange={handleinputchange}/>
+        <Form.Control as="textarea" rows={2} name='address' value={EmployData.address} onChange={handleinputchange} required autoComplete='off'/>
         <Form.Label >Contact Info :</Form.Label>
-        <Form.Control type="text" name="contact" value={EmployData.contact} onChange={handleinputchange}/>
+        <Form.Control type="text" name="contact" value={EmployData.contact} onChange={handleinputchange} required autoComplete='off'/>
         <Form.Label >Choose Department :</Form.Label>
         <Form.Select name='department' value={EmployData.department} onChange={handleinputchange}>
             <option>Department</option>
@@ -73,34 +94,41 @@ function EmpForm(){
             <option value="Active">Active</option>
             <option value="Inactive">Inactive</option>
         </Form.Select>
-        <Button variant="primary" type='submit'>Submit</Button>
+        <Button variant="primary" type='submit'>{EditData !== null ? 'Update' : 'Submit'}</Button>
         </Form>
         </Container>
 
-        <Table striped bordered hover className='mt-4'>
+        <Table striped hover className='mt-4' responsive-sm deletedata={deletedata} >
             <thead>
                 <tr>
+                    <th>No.</th>
                     <th>Name</th>
                     <th>Designation</th>
                     <th>Address</th>
                     <th>Contact Info.</th>
                     <th>Department</th>
                     <th>Status</th>
-                    <th>Delete</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                <TableView SubmitData={SubmitData} deletedata={deletedata}/>
-                {/* {SubmitData.map((data,index) => (
+                {/* <TableView SubmitData={SubmitData} deletedata={deletedata} handleEdit={handleEdit}/> */}
+                {
+                    SubmitData.map((data,index) => (
                     <tr key={index}>
+                        <td>{index+1}</td>
                         <td>{data.name}</td>
                         <td>{data.designation}</td>
                         <td>{data.address}</td>
                         <td>{data.contact}</td>
                         <td>{data.department}</td>
                         <td>{data.status}</td>
+                        <td>
+                            <Button variant="primary" onClick={()=> handleEdit(index)}>Edit</Button>&nbsp;
+                            <Button variant="danger" onClick={() => deletedata(index)}>Delete</Button>
+                        </td>
                     </tr>
-                ))} */}
+                ))}
             </tbody>
         </Table>
     </>
--- src/Components/TableView.js
+++ src/Components/TableView.js
@@ -1,14 +1,27 @@
-import { FaTrashAlt } from "react-icons/fa";
-function Table({SubmitData,deletedata}){
-    return SubmitData.map((data,contact) => (
-        <tr key={contact}>
-            <td>{data.name}</td>
-            <td>{data.designation}</td>
-            <td>{data.address}</td>
-            <td>{data.contact}</td>
-            <td>{data.department}</td>
-            <td>{data.status}</td>
-            <td onClick={() => deletedata(data.contact)}><icon><FaTrashAlt style={{color:'red'}} /></icon></td>
-        </tr>
-))}
-export default Table;
+// import { Button} from "react-bootstrap";
+// import { FaTrashAlt } from "react-icons/fa";
+// import { FaEdit } from "react-icons/fa";
+// import { useState } from "react";
+// function Table({SubmitData,deletedata,handleEdit}){
+    
+    // return (
+    //     SubmitData.map((data,index) => (
+    //         <tr key={index}>
+    //         <td>{index+1}</td>
+    //         <td>{data.name}</td>
+    //         <td>{data.designation}</td>
+    //         <td>{data.address}</td>
+    //         <td>{data.contact}</td>
+    //         <td>{data.department}</td>
+    //         <td>{data.status}</td>
+    //         <td>
+            
+                // <Button variant="primary" onClick={()=> handleEdit(index)}>Edit</Button>&nbsp;
+                // <Button variant="danger" onClick={() => deletedata(index)}>Delete</Button>
+                // </td>
+        // </tr>
+        
+//         )
+// ))}
+
+// export default Table;
